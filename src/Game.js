edb21dab9ead9ff5d38347bdd110351388f36c37
Alex Turpin <alex.turpin@gmail.com>
2019-04-14 22:52:37 -0400

    Prettier dice

--- src/Game.js
+++ src/Game.js
@@ -1,12 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import './Game.css';
 
+import die1 from './die/1.png';
+import die2 from './die/2.png';
+import die3 from './die/3.png';
+import die4 from './die/4.png';
+import die5 from './die/5.png';
+import die6 from './die/6.png';
+const diceImages = [die1, die2, die3, die4, die5, die6];
+
 function Die({ value, locked, onClick }) {
-	const characters = ['⚀', '⚁', '⚂', '⚃', '⚄', '⚅'];
 	return (
-		<span className={`die ${locked ? 'locked' : ''}`} onClick={onClick}>
-			{characters[value - 1]}
-		</span>
+		<img
+			className={`die ${locked ? 'locked' : ''}`}
+			onClick={onClick}
+			src={diceImages[value - 1]}
+			alt={value}
+		/>
 	);
 }
 
